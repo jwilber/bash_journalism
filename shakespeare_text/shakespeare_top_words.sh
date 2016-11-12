50ed895023f6585d1322717b8f44685ee669ebfe
Jared Wilber <jwilber@users.noreply.github.com>
2016-11-12 15:45:45 -0800

    Update shakespeare_top_words.sh

--- shakespeare_text/shakespeare_top_words.sh
+++ shakespeare_text/shakespeare_top_words.sh
@@ -1,24 +1,29 @@
 # -----------------------------------
-# cd into dir holding shakespeare plays
-# -----------------------------------
-cd shakespeare-plays-flat-text
-
-
-# -----------------------------------
 # Count the number of words of at least size '$2' in document '$1'.txt
 # -----------------------------------
+
+
+# test to see if the second argument, minimum number of word chars
+# has been set. If not, it defaults to 1
 if [[ -z $2 ]]; then
   mval=1
 else
   mval=$2
 fi
 
-cat "$1.txt" | \
+# test to see if the third argument, maximum number of word chars
+# has been set. If not, there is no maximum word length
+if [[ -z $3 ]]; then
+  regex="[[:alpha:]]{$mval,}"
+else
+  regex="\b[[:alpha:]]{$mval,$3}\b"
+fi
+
+cat "./shakespeare-plays-flat-text/$1.txt" | \
   tr [[:upper:]] [[:lower:]] | \
-  grep -oE "[[:alpha:]]{$mval,}" | \
+  grep -oE "$regex" | \
   sort | uniq -c | \
   sort -rn | head -n 10
-
 
 
 
