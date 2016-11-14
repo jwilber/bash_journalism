b5a50a4cf349d8012730ae1f1399a3429e8a5fd0
Jared Wilber <jwilber@users.noreply.github.com>
2016-11-13 19:46:39 -0800

    Update collect_wh_briefings.sh
    commented code

--- white_house_briefings/collect_wh_briefings.sh
+++ white_house_briefings/collect_wh_briefings.sh
@@ -1,7 +1,12 @@
+# ---------------
+# This script collects the *listings* of all white house press briefings currently available online
+# ---------------
+
 # As of 13 Novermber 2016 there are 174 pages of briefings
 
 for page_num in $(seq 0 174); do
 	echo "Downloading page $page_num"
 	echo
-	curl http://www.whitehouse.gov/briefing-room/press-briefings?page=$page_num -so wh-briefings/wh_$page_num.html
+	curl http://www.whitehouse.gov/briefing-room/press-briefings?page=$page_num \
+	-so wh-briefings/wh_briefing_page_$page_num.html
 done
